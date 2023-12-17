b2ddca69d86cc7dd70dad2da2234f6590bf692e1
Prajwal Bati <prajwalbati@gmail.com>
2023-12-17 16:04:53 -0500

    login user and todo list

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,88 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const navigate = useNavigate();
+    const [credentials, setCredentials] = useState({
+        email: '',
+        password: ''
+    });
+    const [errors, setErrors] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const onChangeHandler = (e) => {
+        let updatedUser = { ...credentials, [e.target.name]: e.target.value };
+        setCredentials(updatedUser);
+    };
+
+    const loginUser = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            let response = await fetch('https://express-todo-mway.onrender.com/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(credentials)
+            });
+            let resJson = await response.json();
+            setErrors([]);
+            if (!response.ok) {
+                if (response.status === 422) {
+                    setErrors(resJson);
+                }
+            } else {
+                let session = resJson.data;
+                window.localStorage.setItem('accesstoken', session.access_token);
+                window.localStorage.setItem('refreshtoken', session.refresh_token);
+                window.localStorage.setItem('expiresin', session.expires_in);
+                navigate('/');
+            }
+        } catch (error) {
+            console.error(error);
+            setErrors([ {msg: 'SomeThing went wrong.'} ]);
+            setLoading(false);
+        }
+    };
+
+    const getErrorsList = () => {
+        if (errors && errors.length > 0) {
+            let errorElem = errors.map((err, index) => {
+                return <div className="row" key={index}>
+                    <div className="col-sm-12">
+                        <div className="alert alert-danger text-center" role="alert">
+                            {err.msg}
+                        </div>
+                    </div>
+                </div>
+            });
+            return errorElem;
+        }
+        return null;
+    }
+
     return (
         <div className="card-body">
             <h4 className="card-title">Login</h4>
-            <form>
+            <form onSubmit={loginUser}>
+                {getErrorsList()}
                 <div className="row form-group">
                     <label className="col-sm-4 control-label">Email *</label>
                     <div className="col-sm-8">
-                        <input type="email" className="form-control" placeholder='Email Address' required />
+                        <input name="email" value={credentials.email} onChange={onChangeHandler} type="email" className="form-control" placeholder='Email Address' required />
                     </div>
                 </div>
                 <div className="row form-group">
                     <label className="col-sm-4 control-label">Password *</label>
                     <div className="col-sm-8">
-                        <input type="password" className="form-control" placeholder='Password' required />
+                        <input name="password" value={credentials.password} onChange={onChangeHandler} type="password" className="form-control" placeholder='Password' required />
                     </div>
                 </div>
 
                 <div className="row form-group">
                     <div className="col-sm-8 offset-sm-4">
-                        <button className="btn btn-primary">Login</button>
+                        <button type="submit" className="btn btn-primary" disabled={loading?true:false}>{ loading ? 'Logging In' : 'Login' }</button>
                         <span> OR </span>
                         <Link to="/register">Create an Account</Link>
                     </div>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,65 +1,60 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import Input from './Input';
-import ListTodo from './ListTodo';
-class Todo extends Component {
-  state = {
-    todos: []
-  }
-  componentDidMount(){
-    this.getTodos();
-  }
-  getTodos = () => {
-    axios.get('https://express-todo-mway.onrender.com/api/todos')
-      .then(res => {
-        if(res.data){
-          this.setState({
-            todos: res.data
-          })
-        }
-      })
-      .catch(err => console.log(err))
-  }
-  deleteTodo = (id) => {
-    axios.delete(`/api/todos/${id}`)
-      .then(res => {
-        if(res.data){
-          this.getTodos()
-        }
-      })
-      .catch(err => console.log(err))
-  }
-  render() {
-    let { todos } = this.state;
-    return(
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 
-        <div className="card-body">
-          <h4 className="card-title">Awesome Todo list</h4>
-          <div className="add-items d-flex"> <input type="text" className="form-control todo-list-input" placeholder="What do you need to do today?" /> <button className="add btn btn-primary font-weight-bold todo-list-add-btn">Add</button> </div>
-          <div className="list-wrapper">
-              <ul className="d-flex flex-column-reverse todo-list">
-                  <li>
-                      <div className="form-check"> <label className="form-check-label"> <input className="checkbox" type="checkbox" /> For what reason would it be advisable. <i className="input-helper"></i></label> </div> <i className="remove mdi mdi-close-circle-outline"></i>
-                  </li>
-                  <li className="completed">
-                      <div className="form-check"> <label className="form-check-label"> <input className="checkbox" type="checkbox" checked="" /> For what reason would it be advisable for me to think. <i className="input-helper"></i></label> </div> <i className="remove mdi mdi-close-circle-outline"></i>
-                  </li>
-                  <li>
-                      <div className="form-check"> <label className="form-check-label"> <input className="checkbox" type="checkbox" /> it be advisable for me to think about business content? <i className="input-helper"></i></label> </div> <i className="remove mdi mdi-close-circle-outline"></i>
-                  </li>
-                  <li>
-                      <div className="form-check"> <label className="form-check-label"> <input className="checkbox" type="checkbox" /> Print Statements all <i className="input-helper"></i></label> </div> <i className="remove mdi mdi-close-circle-outline"></i>
-                  </li>
-                  <li className="completed">
-                      <div className="form-check"> <label className="form-check-label"> <input className="checkbox" type="checkbox" checked="" /> Call Rampbo <i className="input-helper"></i></label> </div> <i className="remove mdi mdi-close-circle-outline"></i>
-                  </li>
-                  <li>
-                      <div className="form-check"> <label className="form-check-label"> <input className="checkbox" type="checkbox" /> Print bills <i className="input-helper"></i></label> </div> <i className="remove mdi mdi-close-circle-outline"></i>
-                  </li>
-              </ul>
-          </div>
-      </div>
-    )
+const Todo = () => {
+  const navigate = useNavigate();
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    getTodos();
+  }, [])
+
+  const getTodos = async () => {
+    try {
+      let response = await fetch('https://express-todo-mway.onrender.com/api/todos', {
+        method: 'GET',
+        headers: {
+            "Authorization": `Bearer ${window.localStorage.accesstoken}`,
+        },
+      });
+      if (response.status === 401) {
+        navigate("/login");
+        return;
+      }
+      let resJson = await response.json();
+      console.log(resJson.data);
+
+      setTodos(resJson.data);
+    } catch (err) {
+      console.log(err);
+      navigate("/login");
+    }
   }
-}
+
+  return (
+    <div className="card-body">
+        <h4 className="card-title">Awesome Todo list</h4>
+        <div className="add-items d-flex"> <input type="text" className="form-control todo-list-input" placeholder="What do you need to do today?" /> <button className="add btn btn-primary font-weight-bold todo-list-add-btn">Add</button> </div>
+        <div className="list-wrapper">
+            <ul className="d-flex flex-column-reverse todo-list">
+              {todos && todos.length > 0 &&
+                <>
+                {todos.map(todo => {
+                  return <li key={todo._id} className={todo.is_completed?'completed':''}>
+                      <div className="form-check">
+                        <label className="form-check-label">
+                        <input className="checkbox" type="checkbox" checked={todo.is_completed ? true : false} /> {todo.title} <i className="input-helper"></i>
+                        </label>
+                      </div>
+                      <i className="remove mdi mdi-close-circle-outline"></i>
+                    </li>
+                  })}
+                </>
+              }
+            </ul>
+        </div>
+    </div>
+  );
+};
+
 export default Todo;
